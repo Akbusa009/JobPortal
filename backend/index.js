acc0fbb8a45f5ebe08be128286ae3cc7af986f1c
akshaybusa <146056552+Akbusa009@users.noreply.github.com>
2024-08-30 11:13:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-portal-akbusa.netlify.app',
+    origin:'https://job-portal-taupe-ten.vercel.app',
     credentials:true
 }
 
